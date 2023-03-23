15d578a54968bf71f3b5f66fc13005c45c1fa60f
SkalskiP <piotr.skalski92@gmail.com>
2023-03-23 16:29:08 +0100

    test updated requirements.txt

--- groundingdino/util/inference.py
+++ groundingdino/util/inference.py
@@ -1,5 +1,22 @@
+import torch
+
+from groundingdino.models import build_model
+from groundingdino.util.misc import clean_state_dict
+from groundingdino.util.slconfig import SLConfig
+
+
 def preprocess_caption(caption: str) -> str:
     result = caption.lower().strip()
     if result.endswith("."):
         return result
     return result + "."
+
+
+def load_model(model_config_path, model_checkpoint_path):
+    args = SLConfig.fromfile(model_config_path)
+    args.device = "cuda"
+    model = build_model(args)
+    checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
+    model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
+    model.eval()
+    return model
