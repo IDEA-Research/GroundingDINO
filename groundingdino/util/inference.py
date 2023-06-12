96ca504e54efa28f63b507ff58a76a1e0bedc320
Mohamad Al Mdfaa <mohamad.almdfaa@gmail.com>
2023-06-12 22:35:01 +0300

    fix: improve phrases2classes implementation
    This commit improves the phrases2classes implementation by using a regular expression to match sub-phrases in the phrases list. This makes the implementation more accurate and efficient.

--- groundingdino/util/inference.py
+++ groundingdino/util/inference.py
@@ -250,8 +250,10 @@ class Model:
     def phrases2classes(phrases: List[str], classes: List[str]) -> np.ndarray:
         class_ids = []
         for phrase in phrases:
-            try:
-                class_ids.append(classes.index(phrase))
-            except ValueError:
+            for class_ in classes:
+                if class_ in phrase:
+                    class_ids.append(classes.index(class_))
+                    break
+            else:
                 class_ids.append(None)
         return np.array(class_ids)
