bc57328570f7cad3a7602ba7e695426855181a89
Pranjalya Tiwari <pranjalyawarrior@gmail.com>
2023-10-15 15:25:50 +0530

    :wolf: add cuda

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    if False and torch.cuda.is_available() and CUDA_HOME is not None:
+    if torch.cuda.is_available() and CUDA_HOME is not None:
         print("Compiling with CUDA")
         extension = CUDAExtension
         sources += source_cuda
