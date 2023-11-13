eeba084341aaa454ce13cb32fa7fd9282fc73a67
Shiyu <ligary04@hotmail.com>
2023-11-13 13:52:37 +0800

    [fix] replace ema_model with model in demo/test_ap_on_coco (#242)

--- demo/test_ap_on_coco.py
+++ demo/test_ap_on_coco.py
@@ -26,7 +26,7 @@ def load_model(model_config_path: str, model_checkpoint_path: str, device: str =
     args.device = device
     model = build_model(args)
     checkpoint = torch.load(model_checkpoint_path, map_location="cpu")
-    model.load_state_dict(clean_state_dict(checkpoint["ema_model"]), strict=False)
+    model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
     model.eval()
     return model
 
