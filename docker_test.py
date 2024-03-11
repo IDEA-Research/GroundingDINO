c023468fafeb0933d06aa8ba49d96089ce9a6af3
Rohan Manzoor <40537445+teenaxta@users.noreply.github.com>
2024-03-11 13:41:39 +0500

    Added Dockerfile along with a file to test Docker (#307)

---
+++ docker_test.py
@@ -0,0 +1,8 @@
+from groundingdino.util.inference import load_model, load_image, predict, annotate
+import torch
+import cv2
+
+model = load_model("groundingdino/config/GroundingDINO_SwinT_OGC.pyy", "weights/groundingdino_swint_ogc.pth")
+model = model.to('cuda:0')
+print(torch.cuda.is_available())
+print('DONE!')
